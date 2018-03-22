adb9c381c620bde08fa620ea0f6ce38085f3bfeb
William Chen <williamchen16@gmail.com>
2018-03-22 14:51:19 -0700

    code cleanup

--- csv_compare.py
+++ csv_compare.py
@@ -5,56 +5,60 @@ from datetime import timedelta, datetime
 import urllib
 import re
 
+def extract_gia_ids(list):
+	return map(lambda m: m[:10], filter(lambda n: len(n[:10]) == 10 and n[:10].isdigit(), list))
+
+def cmp_new_rows(basefn, currfn, key):
+	base = pd.read_csv(basefn, sep='\t')
+	curr = pd.read_csv(currfn, sep='\t')
+	return curr[~curr[key].isin(base[key])]
+
+def epoch_str(epoch, strftime = '%m-%d-%Y %H:%M'):
+	return datetime.fromtimestamp(int(epoch)).strftime(strftime)
+
+def diff_latest_csvs(root_dir):
+	"""assumes the filenames are the epoch string
+	assumes root_dir exists and ends with slash
+	assumes there's more than two files"""
+	filenames = os.listdir(root_dir)
+	filenames.sort(reverse=True)
+	head_fn, base_fn = filenames[:2]
+	head_path, base_path = map(lambda fn: root_dir + fn, filenames[:2])
+	print('comparing base {} with head {}'.format(epoch_str(base_fn[:10]), epoch_str(head_fn[:10])))
+	return cmp_new_rows(base_path, head_path, 'lab_id')
+
+def filter_rows_by_remained_downloads(tmp_dir, dataframe, url_colname, key_colname):
+	fstruct = lambda p: re.search('(.*)\.(.+)$', os.path.basename(p)).groups()
+	for idx, row in dataframe.iterrows():
+		url, key = row[url_colname], row[key_colname]
+		path = '{}{}.{}'.format(tmp_dir, key, fstruct(url)[1])
+		if not os.path.exists(path):
+			print('downloading {} to {}'.format(url, path))
+			urllib.urlretrieve(url, path)
+	raw_input('After sorting through the files in {}, press Enter to continue...'.format(tmp_dir))
+	keys = map(lambda fpath: fstruct(fpath)[0], os.listdir(tmp_dir))
+	for i in os.listdir(tmp_dir):
+		os.remove(tmp_dir + i)
+	return dataframe[dataframe[key_colname].isin(keys)]
+
+
 def main():
+	filtered = diff_latest_csvs(os.getcwd() + '/bbucutting/')
+	filtered_fn = 'bbucutting-filtered.csv'
+	filtered.to_csv(open(filtered_fn, 'w'), mode='w', header=False, sep='\t')
+	########
 	# now = datetime.now()
 	# day_ago = now - timedelta(days=1)
 	# file_today = "yadav/{}.csv".format(now.strftime('%m-%d-%Y'))
 	# file_yday  = "yadav-{}.csv".format(day_ago.strftime('%m-%d-%Y'))
-	root_dir = os.getcwd() + '/yadav/'
 	gia_dir = os.getcwd() + '/yadav-cert/'
-	filenames = os.listdir(root_dir)
-	filenames.sort(reverse=True)
-
-	prev = pd.read_csv(root_dir + filenames[1], sep='\t')
-	curr = pd.read_csv(root_dir + filenames[0], sep='\t')
-
-	print('comparing {} with {}'.format(filenames[1], filenames[0]))
-
-	filtered = curr[~curr.lab_id.isin(prev.lab_id)]
+	filtered = diff_latest_csvs(os.getcwd() + '/yadav/')
 	if len(filtered) == 0:
 		print('no changes since last file')
 		return
 
-	for idx, row in filtered.iterrows():
-		url = row.cert_url
-		path = gia_dir + os.path.basename(url)
-		if not os.path.exists(path):
-			print('downloading {}'.format(url))
-			urllib.urlretrieve(url, path)
-		else:
-			print('skipping downloading {}'.format(url))
-
-	raw_input('After sorting through the GIA certs, press Enter to continue...')
-
-	ids = extract_gia_ids(os.listdir(gia_dir))
-	filtered = filtered[filtered.lab_id.isin(ids)]
-	
-	for i in os.listdir(gia_dir):
-		os.remove(gia_dir + i)
-
-	for idex, row in filtered.iterrows():
-		url = row.img_url
-		path = gia_dir + str(row.lab_id) + '.jpg'
-		if not os.path.exists(path):
-			print('downloading {}'.format(url))
-			urllib.urlretrieve(url, path)
-		else:
-			print('skipping downloading {}'.format(url))
-		
-	raw_input('After sorting through the diamond images, press Enter to continue...')
-
-	ids = extract_gia_ids(os.listdir(gia_dir))
-	filtered = filtered[filtered.lab_id.isin(ids)]
+	filtered = filter_rows_by_remained_downloads(gia_dir, filtered, 'cert_url', 'lab_id')
+	filtered = filter_rows_by_remained_downloads(gia_dir, filtered, 'img_url', 'lab_id')
 
 	f = open('yadav-filtered.csv', 'w')
 	filtered.to_csv(f, mode='w', header=False, sep='\t')
@@ -62,23 +66,7 @@ def main():
 	for i in os.listdir(gia_dir):
 		os.remove(gia_dir + i)
 
-	#######
 
-	root_dir = os.getcwd() + '/bbucutting/'
-	filenames = os.listdir(root_dir)
-	filenames.sort(reverse=True)
-
-	prev = pd.read_csv(root_dir + filenames[1], sep='\t')
-	curr = pd.read_csv(root_dir + filenames[0], sep='\t')
-
-	print('comparing {} with {}'.format(filenames[1], filenames[0]))
-
-	filtered = curr[~curr.lab_id.isin(prev.lab_id)]
-	f = open('bbucutting-filtered.csv', 'w')
-	filtered.to_csv(f, mode='w', header=False, sep='\t')
-
-def extract_gia_ids(list):
-	return map(lambda m: m[:10], filter(lambda n: len(n[:10]) == 10 and n[:10].isdigit(), list))
 
 if __name__== "__main__":
     main()
